4bd2b7ed9e77623c966b01aa51b94ae80fb75720
Janko Pirih <Janko Pirih>
2016-08-10 17:29:46 +0200

    dashboard_logic.js

--- public/js/dashboard_logic.js
+++ public/js/dashboard_logic.js
@@ -3,9 +3,30 @@
  */
 
 $(document).ready(function () {
-    $('#categoryForm').on('submit', function (e) {
-        e.preventDefault();
 
-        var categoryName = $(this).find('input[name=dca]')
-    })
-})
+var name = $('#category_name').val();
+$('#categoryForm').on('submit', function (e) {
+    e.preventDefault();
+
+    $.ajax({
+        type: 'POST',
+        url: '/categories/new',
+        dataType: 'JSON',
+        data: {
+            _token: $('input[name=_token]').val(),
+            category_name: $('input[name=category_name]').val()
+        },
+        success: function (response) {
+            $('#category_msg').addClass('alert').addClass('alert-success').append(response['msg']);
+            $('#categories').append(
+                '<tr>'
+                +'<td>' + response['category_id'] +'</td>'
+                +'<td>'+ $('input[name=category_name]').val() + '</td>'
+                +'</tr>'
+            )
+        }
+    });
+});
+
+
+});
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -243,34 +243,6 @@
 
 <!-- javascript -->
 <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script>
-    $(document).ready(function () {
-
-        var name = $('#category_name').val();
-        $('#categoryForm').on('submit', function (e) {
-            e.preventDefault();
-
-            $.ajax({
-                type: 'POST',
-                url: '{{route('new_category')}}',
-                dataType: 'JSON',
-                data: {
-                    _token: $('input[name=_token]').val(),
-                    category_name: $('input[name=category_name]').val()
-                },
-                success: function (response) {
-                   $('#category_msg').addClass('alert').addClass('alert-success').append(response['msg']);
-                    $('#categories').append(
-                            '<tr>'
-                            +'<td>' + response['category_id'] +'</td>'
-                            +'<td>'+ $('input[name=category_name]').val() + '</td>'
-                            +'</tr>'
-                    )
-                }
-            });
-        });
-
-
-    });
+<script src="/js/dashboard_logic.js" type="text/javascript">
 
 </script>
